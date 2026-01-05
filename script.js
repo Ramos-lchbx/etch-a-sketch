357d4a0f2b99126fc1786ad395b0f19f78797f25
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-01-05 17:12:12 -0600

    Add resize button
    This button allows the user to select a number of pixels per side.

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function colorChange(div){
     div.style.backgroundColor = "blue"
 }
 
+let sizePrompt = document.querySelector("button")
+sizePrompt.addEventListener("click", () => newSize())
+
+function newSize(){
+    let userSize = prompt("Enter a number of pixels per side, smaller than 100", "");
+
+    if (userSize <= 100){
+
+        container.innerHTML = "";
+
+        for (let i= 0 ; i < userSize ** 2 ; i++){
+        const div = document.createElement("div");
+        container.appendChild(div);
+        div.style.backgroundColor = "red";
+        div.style.width = `${800/userSize}px`;
+        div.style.height = `${800/userSize}px`;
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "blue"; 
+            });
+        }
+    }
+    else alert("TOO BIG, DUMMY");
+
+    
+
+    
+}
+
+for (let i= 0 ; i < newSize ** 2 ; i++){
+
+}
+
