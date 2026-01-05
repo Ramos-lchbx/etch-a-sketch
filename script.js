1104c69a3ced06adca5c8917f2a91cf45154defa
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-01-05 17:43:55 -0600

    Add random colors

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ let container = document.querySelector(".container");
 for (let i = 0 ; i < 256 ; i++){
     const div = document.createElement("div");
     container.appendChild(div);
-    div.style.backgroundColor = "red";
+
+    const rgbValue = getRandomRgb();
+
+    div.style.backgroundColor = "rgb(100, 165, 221)";
     div.style.width = "50px";
     div.style.height = "50px";
     div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = "blue"; 
+    div.style.backgroundColor = `${rgbValue}`; 
 });
+
+
+function getRandomRgb(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`
+}
+
 }
 
 function colorChange(div){
@@ -28,11 +41,12 @@ function newSize(){
         for (let i= 0 ; i < userSize ** 2 ; i++){
         const div = document.createElement("div");
         container.appendChild(div);
-        div.style.backgroundColor = "red";
+        const rgbValue = getRandomRgb();
+        div.style.backgroundColor = "rgb(100, 165, 221)";
         div.style.width = `${800/userSize}px`;
         div.style.height = `${800/userSize}px`;
         div.addEventListener("mouseenter", () => {
-            div.style.backgroundColor = "blue"; 
+            div.style.backgroundColor = `${rgbValue}`; 
             });
         }
     }
