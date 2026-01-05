6de8dc6faa750c6de9883108472bd77424c4150c
Ramos-lchbx <alejandroramos181298@hotmail.com>
2026-01-05 16:30:27 -0600

    Add basic functionality
    Add the ability to change some elements' color by hovering over them.

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ for (let i = 0 ; i < 256 ; i++){
     const div = document.createElement("div");
     container.appendChild(div);
     div.style.backgroundColor = "red";
-    div.style.width = "48px";
-    div.style.height = "48px";
-    div.style.margin = "1px"
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.addEventListener("mouseenter", () => {
+    div.style.backgroundColor = "blue"; 
+});
 }
+
+function colorChange(div){
+    div.style.backgroundColor = "blue"
+}
+
